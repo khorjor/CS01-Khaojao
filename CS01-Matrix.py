37719d5241e3bd8c273ec389a5792aedc6697dc4
khorjor <87053833+khorjor@users.noreply.github.com>
2021-09-28 10:14:19 +0700

    Update Matrix

---
+++ CS01-Matrix.py
@@ -0,0 +1,26 @@
+import numpy as np
+MatrixOne = []
+MatrixTwo = []
+loop = 1 
+MatrixColumns = int(input('EnterYourColumns : '))
+MatrixRows = int(input("EnterYourRows : "))
+while(loop <= 2):
+    print("EnterYourMatrix",loop)
+    for i in range(MatrixRows) :
+        for j in range(MatrixColumns) :
+        print("MatrixColumns [",i,",",j,"] :",end ="")
+        if(loop == 1):
+            MatrixOneInput = int(input(""))
+            MatrixOne.append(MatrixOneInput)
+        else:
+            MatrixTwoInput = int(input("")) 
+            MatrixTwo.append(MatrixTwoInput)
+    loop += 1
+NumpyMatrixOne = np.asarray(MatrixOne)
+NumpyMatrixTwo = np.asarray(MatrixTwo)
+NewMatrixOne=NumpyMatrixOne.reshape(MatrixRows,MatrixColumns)
+NewMatrixTwo=NumpyMatrixTwo.reshape(MatrixRows,MatrixColumns)
+print(NewMatrixOne,"\n=========================")
+print(NewMatrixTwo,"\n=========================")
+sum = np.add(NewMatrixOne, NewMatrixTwo)
+print(sum)
